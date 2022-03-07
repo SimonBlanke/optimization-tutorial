46ce474bf037fdcc8fb52a8eeb9a7684407e4272
Simon Blanke <simon.blanke@yahoo.com>
2022-03-07 18:18:40 +0100

    change name

--- optimization_tutorial/optimizers/optimizer_template.py
+++ optimization_tutorial/optimizers/optimizer_template.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 def optimizer_app(app_d):
     st.sidebar.write("")
-    radio_select = st.sidebar.radio("Parameters", options=list(app_d.keys()))
+    radio_select = st.sidebar.radio("Algorithm details:", options=list(app_d.keys()))
 
     _app_, args_d = app_d[radio_select]
     _app_(args_d)
