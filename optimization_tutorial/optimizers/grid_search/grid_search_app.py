7b848c24f9c3314ca3d33e54cc09b93f95b88c64
Simon Blanke <simon.blanke@yahoo.com>
2022-04-30 19:48:37 +0200

    finish grid search page

--- optimization_tutorial/optimizers/grid_search/grid_search_app.py
+++ optimization_tutorial/optimizers/grid_search/grid_search_app.py
@@ -41,7 +41,10 @@ info_ = """
 
 
 implementation_ = """
-...
+The implementation of this grid-search was realized by
+[Thomas Gak-Deluen](https://github.com/tgdn) and his team. The algorithm
+works by choosing a direction in the beginning and moving through the search space
+one 2-dimensional-plane at a time.
 """
 
 overview_app_args_d = {
